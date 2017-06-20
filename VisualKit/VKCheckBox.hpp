21b6aae9a7bb1cfc7513b58753846d91c9bdb7c4
manu88 <manuel.deneu@gmail.com>
2017-06-20 18:30:20 +0200

    Added Checkbox widget

---
+++ VisualKit/VKCheckBox.hpp
@@ -0,0 +1,14 @@
+//
+//  VKCheckBox.hpp
+//  VisualKit
+//
+//  Created by Manuel Deneu on 20/06/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#ifndef VKCheckBox_hpp
+#define VKCheckBox_hpp
+
+#include <stdio.h>
+
+#endif /* VKCheckBox_hpp */
