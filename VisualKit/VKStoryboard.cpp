db14a7223b9d863c1d19fcfe8593334a82988866
manu88 <manuel.deneu@gmail.com>
2017-06-20 09:39:15 +0200

    Added Stoyboard parser unit

---
+++ VisualKit/VKStoryboard.cpp
@@ -0,0 +1,9 @@
+//
+//  VKStoryboard.cpp
+//  VisualKit
+//
+//  Created by Manuel Deneu on 20/06/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#include "VKStoryboard.hpp"
---
+++ include/VKStoryboard.hpp
@@ -0,0 +1,14 @@
+//
+//  VKStoryboard.hpp
+//  VisualKit
+//
+//  Created by Manuel Deneu on 20/06/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#ifndef VKStoryboard_hpp
+#define VKStoryboard_hpp
+
+#include <stdio.h>
+
+#endif /* VKStoryboard_hpp */
